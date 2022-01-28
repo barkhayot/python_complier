38506eb0eeee27e8500467a8bb9ed3b94776f212
Barkhayot Juraev <coderviewer@gmail.com>
2022-01-28 17:19:41 +0900

    update

--- EditorCode/settings.py
+++ EditorCode/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%4*+)ij2=itvrwsm*^hnoym*o3vwx@)!dqd1gxm$&t72gi1gz+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
